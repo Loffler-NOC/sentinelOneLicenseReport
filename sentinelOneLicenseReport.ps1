65ccbb7affd9e40374f72e72ca51b7816836ae5f
Loffler-NOC <150292433+Loffler-NOC@users.noreply.github.com>
2024-03-06 07:41:43 -0600

    Create CSV
    Changing script to output a csv instead of writing output to console

--- sentinelOneLicenseReport.ps1
+++ sentinelOneLicenseReport.ps1
@@ -5,11 +5,14 @@ $uri = "$env:S1SiteURI"
 $apiUri = "/web/api/v2.1/sites"
 
 $body = @{
-    apiToken = “$env:S1ReportingApiToken”
+    apiToken = "$env:S1ReportingApiToken"
     sortBy = "name"
 }
 
 $uri += $apiUri
+
+# Create an array to hold site data
+$sitesData = @()
 
 $response = Invoke-RestMethod -Uri $uri -Body $body
 
@@ -19,24 +22,30 @@ do {
 
     # Loop through each site and extract required data
     foreach ($site in $sites) {
-        $accountName = $site.name
-        $activeLicenses = $site.activeLicenses
-        $totalLicenses = $site.totalLicenses
+        $siteData = [PSCustomObject]@{
+            Site = $site.name
+            ActiveLicenses = $site.activeLicenses
+            TotalLicenses = $site.totalLicenses
+        }
 
-        # Output required data
-        Write-Output "Site: $accountName"
-        Write-Output "Active Licenses: $activeLicenses"
-        Write-Output "Total Licenses: $totalLicenses"
-        Write-Output ""
+        # Add site data to the array
+        $sitesData += $siteData
     }
 
     $nextCursor = $response.pagination.nextCursor
     $body = @{
-        apiToken   = “$env:S1ReportingApiToken”
+        apiToken = "$env:S1ReportingApiToken"
         cursor = $nextCursor
         sortBy = "name"
     }
     $response = Invoke-RestMethod -Uri $uri -Body $body
-} while (
-    $null -ne $nextCursor
-)
+} while ($null -ne $nextCursor)
+
+# Check if the directory exists, if not, create it
+$directory = "C:\Temp"
+if (!(Test-Path $directory)) {
+    New-Item -Path $directory -ItemType Directory
+}
+
+# Export data to CSV
+$sitesData | Export-Csv -Path "C:\Temp\sites_data.csv" -NoTypeInformation
